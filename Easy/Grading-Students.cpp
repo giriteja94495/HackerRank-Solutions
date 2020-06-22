e67270f212e0a53a82d8b5af518d15bf2a0b9bbc
GiriTejaBille <giriteja94495@gmail.com>
2020-06-22 11:53:42 +0530

    Create Grading-Students.cpp

---
+++ Easy/Grading-Students.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+vector<int> gradingStudents(vector<int> grades) {
+vector<int> giri;
+for(auto a:grades){
+    if(a<38) giri.push_back(a);
+    else{
+        if(a%5==0) giri.push_back(a);
+        else {
+            int next_pow=(a/5)+1;
+            if(next_pow*5 -a <3) giri.push_back(5*next_pow);
+            else giri.push_back(a);
+        }
+
+    }
+    
+}
+return giri;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string grades_count_temp;
+    getline(cin, grades_count_temp);
+
+    int grades_count = stoi(ltrim(rtrim(grades_count_temp)));
+
+    vector<int> grades(grades_count);
+
+    for (int i = 0; i < grades_count; i++) {
+        string grades_item_temp;
+        getline(cin, grades_item_temp);
+
+        int grades_item = stoi(ltrim(rtrim(grades_item_temp)));
+
+        grades[i] = grades_item;
+    }
+
+    vector<int> result = gradingStudents(grades);
+
+    for (int i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
